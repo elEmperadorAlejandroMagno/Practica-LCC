c8d164a7814fa818a78e51bdac7ba3cd32c671fb
Alejandro el Emperador <144720067+elEmperadorAlejandroMagno@users.noreply.github.com>
2026-04-10 00:01:40 -0300

    Implement initial structure for betting processing
    Add comments and initial function for processing bets

--- AyRP/Ejercicio14_u3.c
+++ AyRP/Ejercicio14_u3.c
@@ -1,3 +1,11 @@
+/*
+Ejercicio 14
+Se procesa la información de N apuestas realizadas en una agencia de quiniela. Por cada apuesta se
+ingresa: el número y el importe.
+Se pide realizar un algoritmo usando subprogramas que permita:
+1. Indicar el número de las apuestas con importes entre dos valores ingresados por teclado.
+2. Escribir el número y el importe de las apuestas mayores a $5000.
+*/
 #include <stdio.h>
 
 int obtener_num_apuesta() {
