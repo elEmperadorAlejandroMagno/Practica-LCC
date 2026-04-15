0564b404f01bea95f43eaead1aea824f2098c334
Alejandro Marvich <alejandromarvich181100@gmail.com>
2026-04-14 22:18:30 -0300

    ejercicio 10 updated

--- AyRP/Ejercicio10_u3.c
+++ AyRP/Ejercicio10_u3.c
@@ -4,42 +4,35 @@ Realizar un algoritmo que determine si un carácter ingresado por el usuario es 
 los anteriores.
 */
 # include <stdio.h>
-# include <string.h>
 
 char evaluar_caracter(char input) {
 	char tipo;
-	char vocales[] = "aAeEiIoOuU";
-	int largo = strlen(vocales);
 
-	tipo = 'N';
-
-	if (input >= '0' && input <= '9') {
+	if ((input >= '0') && (input <= '9')) {
 		tipo = 'D';
-		return tipo;
-	}
+	} else if(((input >= 'A') && (input <= 'Z')) || ((input >= 'a') && (input <= 'z'))) {
+		tipo = 'V';
+	} else tipo = 'N';
 
-	for (int i = 0; i < largo; i++) {
-		if (input == vocalesMay[i]) {
-			tipo = 'V';
-		}	
-	}	
 return tipo;
 }
-
+void mostrar_tipo(char tipo, char caracter) {
+	switch (tipo) {
+		case 'V':
+			printf("El caracter '%c' es una vocal\n", caracter);
+			break;
+		case 'D':
+			printf("El caracter '%c' es un dígito\n", caracter);
+			break;
+		default:
+			printf("El caracter '%c' no es ni vocal ni dígito\n", caracter);
+	}
+}
 int main() {
 	char input, tipo;
 	printf("Ingresar un caracter: ");
 	scanf("%c", &input);
-
 	tipo = evaluar_caracter(input);
-	switch (tipo) {
-		case 'V':
-			printf("Es una vocal\n");
-			break;
-		case 'D':
-			printf("Es un dígito\n");
-			break;
-		default:
-			printf("No es ni vocal ni dígito\n");
-	}
+	mostrar_tipo(tipo, input);
+	return 0;
 }
