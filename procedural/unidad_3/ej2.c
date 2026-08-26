11fe91d5a7bd48625877788663055411e3e8fa3b
Alejandro Marvich <alejandromarvich181100@gmail.com>
2026-08-26 13:29:12 -0300

    ej2 u3 completo

--- procedural/unidad_3/ej2.c
+++ procedural/unidad_3/ej2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define N 10 // original 150
+#define N 4 // original 150
 
 typedef struct
 {
@@ -24,7 +24,7 @@ int main()
     product products[N];
     int last_0_stock;
     initProducts(products);
-    printf("Se ha recaudado un total de: $%.2f", salesProcessor(products));
+    printf("Se ha recaudado un total de: $%.2f\n", salesProcessor(products));
     sortProductsByStockASC(products);
     showProductsNoStock(getProductsNoStock(products, &last_0_stock));
     showActualStockCosts(getActualStockCosts(products, last_0_stock));
@@ -33,24 +33,26 @@ int main()
 
 void initProducts(product arr[])
 {
+    printf("Ingresar datos de productos en stock\n");
     for(int i = 0; i < N; i++)
     {
-        printf("Product details");
+        printf("Product details\n");
         printf("Code: ");
         // asumir que el codigo del producto es único
-        scanf("%d", &arr[i].code);
-        printf("name: ");
+        scanf(" %d", &arr[i].code);
+        getchar();
+        printf("name:");
         fgets(arr[i].name, sizeof(arr[i].name), stdin);
         arr[i].name[strcspn(arr[i].name, "\n")] = '\0';
         do 
         {
             printf("Price per unit: $");
-            scanf("%f", &arr[i].u_price); 
+            scanf(" %f", &arr[i].u_price); 
         }while(arr[i].u_price <= 0);
         do 
         {
             printf("stock: ");
-            scanf("%d", &arr[i].stock);
+            scanf(" %d", &arr[i].stock);
         }while (arr[i].stock < 0);
     }
     return;
@@ -60,12 +62,14 @@ float salesProcessor(product arr[])
 {
     int targ, units, i;
     float acum = 0;
+
+    printf("Ventas realizadas\n");
     printf("Product code: ");
-    scanf("%d", &targ);
+    scanf(" %d", &targ);
     while(targ != 0)
     {
         printf("Units sold: ");
-        scanf("%d", &units);
+        scanf(" %d", &units);
         i = 0;
         while(i < N && arr[i].code != targ)
         {
@@ -75,9 +79,9 @@ float salesProcessor(product arr[])
         {
             arr[i].stock -= units;
             acum += (arr[i].u_price * units);
-        } else printf("Producto no encontrado o no hay suficiente stock");
+        } else printf("Producto no encontrado o no hay suficiente stock\n");
         printf("Product code: ");
-        scanf("%d", &targ);
+        scanf(" %d", &targ);
     };
     return acum;
 };
@@ -105,13 +109,13 @@ void sortProductsByStockASC(product arr[])
 
 void showProductsNoStock(int num)
 {
-    printf("Cantidad de productos sin stock: %d", num);
+    printf("Cantidad de productos sin stock: %d\n", num);
     return;
 };
 
 void showActualStockCosts(float amount)
 {
-    printf("Costo actual del stock acumulado: $%.2f", amount);
+    printf("Costo actual del stock acumulado: $%.2f\n", amount);
     return;
 };
 
