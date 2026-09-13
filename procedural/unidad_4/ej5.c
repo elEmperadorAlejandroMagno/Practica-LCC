9bcbc2cdc5ef8746cb0bdf003bad711ad278a88b
Alejandro Marvich <alejandromarvich181100@gmail.com>
2026-09-13 14:29:30 -0300

    ej5 unidad 4 punto d completado

--- procedural/unidad_4/ej5.c
+++ procedural/unidad_4/ej5.c
@@ -5,19 +5,20 @@
 void loadVoteArr(int arr[], int i);
 void getMaxMin(int arr[], int *min, int *max, int i, int *i_min, int *i_max);
 void getEvaluation(int arr[], int *less5k, int *more9k, int i);
-void getAvgByLanguage(int arr[], int i);
+void getLanguageAboveAvg(int arr[], int i, int avg);
+void getAvg(int a[], int *acum, int i);
 
 const char *LANGUAGES[] = {"Javascript", "HTML", "CSS", "Java", "Python", "C"};
 
 int main()
 {
     int arr_votes[L];
-    int min = 99999, max = 0, c_less5k = 0, c_more9k = 0;
+    int min = 99999, max = 0, c_less5k = 0, c_more9k = 0, acum = 0;
     int i_min, i_max;
     loadVoteArr(arr_votes, 0);
     getMaxMin(arr_votes, &min, &max, 0, &i_min, &i_max);
     getEvaluation(arr_votes, &c_less5k, &c_more9k, 0);
-    getAvgByLanguage(arr_votes, 0);
+    getAvg(arr_votes, &acum, 0);
     return 0;
 };
 
@@ -76,6 +77,32 @@ void getEvaluation(int arr[], int *less5k, int *more9k, int i)
     };
 };    
 
-void getAvgByLanguage(int arr[], int i)
+void getAvg(int arr[], int *acum, int i)
 {
-}
+    int avg;
+    if(i < L)
+    {
+        *acum += arr[i];
+        getAvg(arr, acum, i + 1);
+        return;
+    }else
+    {
+        avg = *acum / L;
+        printf("Promedio de votos: %d\n",avg);
+        getLanguageAboveAvg(arr, 0, avg);
+        return;
+    }
+};
+
+void getLanguageAboveAvg(int arr[], int i, int avg)
+{
+    if(i < L)
+    {
+        if(arr[i] > avg)
+        {
+            printf("%s esta por encima del promedio de votos\n", LANGUAGES[i]);
+        }
+        getLanguageAboveAvg(arr, i + 1, avg);
+        return;
+    } else return;
+};
