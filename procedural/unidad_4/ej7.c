64ee420f92d4157303b4b067f0b9834b363d8621
Alejandro Marvich <alejandromarvich181100@gmail.com>
2026-09-13 21:55:11 -0300

    ej7 casi completo, falta la carga de productos con arreglo dinamico?

--- procedural/unidad_4/ej7.c
+++ procedural/unidad_4/ej7.c
@@ -9,18 +9,27 @@ typedef struct
 
 void getMaxPrice(p_det product[], int i, float *max_price, int P);
 int loadProducts(p_det arr[], int *P);
-void getAvgPrice(p_det arr[], int i, float *avg, int P);
-int saveProductBelowAvg(p_det arr[], int i, p_det **arrb, int P, int *j);
+float getAvgPrice(p_det arr[], int i, float *avg, int P);
+void saveProductBelowAvg(p_det arr[], int i, p_det **arrb, int P, int *j, float avg);
+void getMinMaxArrB(p_det **arr, int i, int j, float *min, float *max);
 
 int main()
 {
-    p_det *arr, *arrb;
+    p_det arr[3] = {{5.90, 1}, {2, 3}, {1.50, 1}};
+    p_det *arrb = malloc(sizeof(p_det));
     int P = 0, j = 0;
-    float max_price = 0;
-    loadProducts(arr, &P);
+    float max_price = 0, avg, max = 0, min = 99999;
+    //loadProducts(arr, &P);
+    P = 3;
     getMaxPrice(arr, 0, &max_price, P);
-    saveProductBelowAvg(arr, 0, &arrb, P, &j);
+    avg = getAvgPrice(arr, 0, &avg, P); 
+    saveProductBelowAvg(arr, 0, &arrb, P, &j, avg);
     // calcular minimo y maximo
+    getMinMaxArrB(&arrb, 0, j, &min, &max);
+    printf("Precio máximo por debajo del precio promedio: %.2f\n", max);
+    printf("Precio mínimo por debajo del precio promedio: %.2f\n", min);
+    //free(arr);
+    free(arrb);
     return 0;
 };
 
@@ -35,23 +44,53 @@ void getMaxPrice(p_det product[], int i, float *max_price, int P)
         getMaxPrice(product, i + 1, max_price, P);
         if(product[i].price == *max_price)
         {
-            printf("Producto %d coincide con el precio máximo registrado de %.2f", i + 1, *max_price);
+            printf("Producto %d coincide con el precio máximo registrado de %.2f\n", i + 1, *max_price);
         }
         return;
     } else return;
 };
 
-void getAvgPrice(p_det arr[], int i, float *avg, int P)
+float getAvgPrice(p_det arr[], int i, float *avg, int P)
 {
     if(i < P)
     {
         *avg += arr[i].price;
-        getAvgPrice(arr, i + 1, avg, P);
-        return;
-    } else return;
+        return getAvgPrice(arr, i + 1, avg, P);
+    } else return (*avg / P);
 };
 
-int saveProductBelowAvg(p_det arr[], int i, p_det **arrb, int P, int *j)
+void saveProductBelowAvg(p_det arr[], int i, p_det **arrb, int P, int *j, float avg)
 {
-    return 1;
+    if(i < P)
+    {
+        saveProductBelowAvg(arr, i + 1, arrb, P, j, avg);
+        if(arr[i].price < avg)
+        {
+            if(*j > 0)
+            {
+                p_det *tmp = malloc((*j + 1) * sizeof(p_det));
+                for(int k = 0; k < *j; k++)
+                {
+                    tmp[k] = (*arrb)[k];
+                }
+                tmp[*j] = arr[i];
+                free(*arrb);
+                *arrb = tmp;
+            } else *arrb[*j] = arr[i];
+            *j += 1;
+        }
+        return;
+    }
+    return;
+};
+
+void getMinMaxArrB(p_det **arr, int i, int j, float *min, float *max)
+{
+    if(i < j)
+    {
+        if((*arr)[i].price < *min) *min = (*arr)[i].price;
+        if((*arr)[i].price > *max) *max = (*arr)[i].price;
+        getMinMaxArrB(arr, i + 1, j, min, max);
+        return;
+    } return;
 };
