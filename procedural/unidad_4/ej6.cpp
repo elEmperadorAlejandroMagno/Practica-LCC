481e3a4bbe03d0e26d019bb869bfa3006f482c7a
Alejandro Marvich <alejandromarvich181100@gmail.com>
2026-09-16 11:10:49 -0300

    ej6 unidad 4 comienzo

---
+++ procedural/unidad_4/ej6.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define T 5 // 30
+
+void loadTemp(int i);
+void loadNewArr(int i, int *j);
+void searchTemp(int i, int targ);
+void binarySearchTemp(int &left, int &right, int &mid, int targ);
+
+int main()
+{
+    return 0;
+};
+
